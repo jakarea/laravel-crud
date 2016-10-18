c33cf017ea477bab9bffb05dca8f7c91f7513226
jakarea <jakareaparvez@gmail.com>
2016-10-18 18:23:07 +0600

    adminController created

---
+++ resources/views/admin/partial/sidenav.blade.php
@@ -0,0 +1,8 @@
+<div id="sidebar-left" class="span2">
+	<div class="nav-collapse sidebar-nav">
+		<ul class="nav nav-tabs nav-stacked main-menu">
+			<li><a href="{!! url('admin') !!}"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+			<li><a href="{!! url('admin/job-post')!!}"><i class="icon-tasks"></i><span class="hidden-tablet"> Post a job</span></a></li>
+		</ul>
+	</div>
+</div>
